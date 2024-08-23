eee5d5487cb97e3c273c5fba26f1df832eb8f4bc
Elton Cavele <82831720+elton971@users.noreply.github.com>
2024-08-23 22:17:43 +0200

    feat: create home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-wrap lg:flex-row min-h-screen antialised">
     <topbar class="w-full fixed top-0 left-0 z-30" />
-    <nuxt class="flex flex-col mx-auto container" />
+    <main class="flex flex-col mx-auto container px-5">
+      <nuxt />
+    </main>
   </div>
 </template>
 
