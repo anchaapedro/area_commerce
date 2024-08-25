c87ebc1b29427c4a3308456f4d0c61bd6cbef3ef
Ancha Pedro <abiche@technoplus.co.mz>
2024-08-25 23:01:33 +0200

    refactor: change button color

--- components/layout/Topbar.vue
+++ components/layout/Topbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-gray-800 p-4">
+  <nav class="bg-gray-800 p-6">
     <div class="container mx-auto flex justify-between items-center">
       <div class="text-white text-xl font-bold">
         <router-link to="/">MEGAMART</router-link>
@@ -8,7 +8,7 @@
       <div class="flex space-x-4">
         <router-link
           to="/sign-up"
-          class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 flex flex-row gap-3"
+          class="bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 flex flex-row gap-3"
           active-class="bg-blue-600"
         >
         <login />
@@ -16,7 +16,7 @@
         </router-link>
         <router-link
           to="/sign-up"
-          class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 flex flex-row gap-3"
+          class="bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 flex flex-row gap-3"
           active-class="bg-blue-600"
         >
         <shopping />
