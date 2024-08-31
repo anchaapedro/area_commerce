f7c5a1ed210d184c8aeec8ba6717c1869e5d43c6
Elton Cavele <82831720+elton971@users.noreply.github.com>
2024-08-31 15:49:06 +0200

    register

--- components/common/inputs/TextInput.vue
+++ components/common/inputs/TextInput.vue
@@ -1,142 +1,86 @@
 <template>
-  <ValidationProvider v-slot="{ errors }" :name="name" :rules="rules">
-    <div class="flex flex-col gap-1">
-      <span
-        v-if="label"
-        id="label"
-        class="flex flex-row items-center select-none"
-        >{{ label }}
-        <strong v-if="required" class="text-primary-default ml-1"
-          >*</strong
-        ></span
-      >
-      <div class="flex flex-row w-full relative">
-        <div class="absolute left-2 px-2 py-2 h-10 z-10">
-          <slot name="icon" />
-        </div>
-        <input
-          v-model="inputValue"
-          class="w-full relative py-1.5 px-2 ring-1 ring-tertiary-light ring-opacity-20 focus:outline-none"
-          :class="[setStyle, { 'pr-10': type === 'password', 'pl-10': icon }]"
-          :type="setType"
-          :min="min"
-          :max="max"
-          :name="name"
-          :rules="rules"
-          :placeholder="placeholder"
-          :disabled="disabled"
-          :readonly="readonly"
-          @blur="$emit('blur', inputValue)"
-          @focus="$emit('focus', inputValue)"
-        />
-        <eye-outline
-          v-if="type === 'password'"
-          id="toggle-Password"
-          class="absolute right-0 py-3 h-10 w-10 cursor-pointer"
-          @click.native="hiddenPassword = !hiddenPassword"
-        />
-      </div>
-      <span v-if="errors[0]" class="text-primary-default text-xs">{{
-        errors[0]
-      }}</span>
-      <span
-        v-if="description"
-        class="flex text-xs text-primary-light text-opacity-50"
-        >{{ description }}</span
-      >
+  <div class="flex flex-col mb-3">
+    <span id="label" class="mb-2">{{ label }}</span>
+    <div class="relative w-full">
+      <input
+        v-model="inputValue"
+        class="w-full py-2 px-4 outline-none"
+        :class="{
+          'border rounded bg-gray-100 border-gray-300 focus:border-2  focus:border-primary-default':
+            isBorder,
+          'cursor-not-allowed text-gray-400': disabled,
+        }"
+        :type="type"
+        :min="min"
+        :max="max"
+        :disabled="disabled"
+        :placeholder="placeholder"
+      />
+      <slot v-if="icon"></slot>
     </div>
-  </ValidationProvider>
+    <span v-if="!!error" id="error" class="text-red-500 text-xs py-2">{{
+      error
+    }}</span>
+  </div>
 </template>
 
 <script lang="ts">
-import eyeOutline from '~/assets/icons/eye-outline.vue'
-export default {
-  name: 'TextInput',
-  components: { eyeOutline },
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TextInput",
+  components: {},
   props: {
     label: {
       type: String,
-      default: '',
-    },
-    value: {
-      type: [Number, String],
-      default: '',
-    },
-    name: {
-      type: String,
-      default: '',
-    },
-    rules: {
-      type: String,
-      default: '',
+      default: "",
     },
     placeholder: {
       type: String,
-      default: '',
+      default: "",
     },
-    customStyle: {
-      type: String,
-      default: 'bg-quaternary-default',
-    },
-    description: {
-      type: String,
-      default: '',
+    value: {
+      type: [Number, String],
+      default: "",
     },
     type: {
       type: String,
-      default: 'text',
-    },
-    rounded: {
-      type: String,
-      default: 'rounded-sm',
+      default: "text",
     },
     disabled: {
       type: Boolean,
       default: false,
     },
+    error: {
+      type: String,
+      default: "",
+    },
     icon: {
       type: Boolean,
       default: false,
     },
-    readonly: {
-      type: Boolean,
-      default: false,
-    },
-    required: {
-      type: Boolean,
-      default: false,
-    },
     min: {
-      type: Number,
-      default: 0,
+      type: String,
+      default: "",
     },
     max: {
-      type: Number,
-      default: null,
+      type: String,
+      default: "",
+    },
+    isBorder: {
+      type: Boolean,
+      default: true,
     },
   },
-  data: () => ({
-    hiddenPassword: true,
-  }),
   computed: {
     inputValue: {
-      get(this: any) {
-        return this.value
+      get(): any {
+        return this.value;
       },
-      set(this: any, value: any) {
-        this.$emit('input', value)
+      set(value: any) {
+        this.$emit("input", value);
       },
-    },
-    setType(this: any) {
-      if (this.type === 'password' && this.hiddenPassword) return 'password'
-      if (this.type === 'password' && !this.hiddenPassword) return 'text'
-      return this.type
-    },
-    setStyle(this: any) {
-      const style = {} as any
-      style[`${this.customStyle} ${this.rounded}`] = true
-      return style
     },
   },
-}
+});
 </script>
--- plugins/axios.js
+++ plugins/axios.js
@@ -3,69 +3,21 @@ export default function ({ $axios }, inject) {
   const mainApi = $axios.create({
     headers: {
       common: {
-        Accept: 'text/plain, */*',
+        Accept: "text/plain, */*",
       },
     },
-  })
+  });
 
-  const accessToken = localStorage.getItem('auth.Bearerlocal')
-    ? localStorage.getItem('auth.Bearerlocal')
-    : null
+  const accessToken = localStorage.getItem("auth.Bearerlocal")
+    ? localStorage.getItem("auth.Bearerlocal")
+    : null;
 
   if (accessToken) {
-    mainApi.defaults.headers.common.Authorization = accessToken
+    mainApi.defaults.headers.common.Authorization = accessToken;
   }
 
-  mainApi.setBaseURL(process.env.MAIN_API_HOST)
+  mainApi.setBaseURL(process.env.MAIN_API_HOST);
 
   // Inject to context as processApi
-  inject('mainApi', mainApi)
-
-  // Authentication api custom axios instance
-  const authApi = $axios.create({
-    headers: {
-      common: {
-        Accept: 'text/plain, */*',
-      },
-    },
-  })
-
-  authApi.setBaseURL(process.env.AUTH_API_HOST)
-
-  // Inject to context as $authApi
-  inject('authApi', authApi)
-
-  // Dynamic forms api custom axios instance
-  const dynamicFormsApi = $axios.create({
-    headers: {
-      common: {
-        Accept: 'text/plain, */*',
-      },
-    },
-  })
-
-  dynamicFormsApi.defaults.headers.common.Authorization = accessToken
-
-  dynamicFormsApi.setBaseURL(process.env.DYNAMIC_FORMS_API_HOST)
-
-  // Inject to context as $authApi
-  inject('dynamicFormsApi', dynamicFormsApi)
-
-  // entity api custom axios instance
-  const entitiesApi = $axios.create({
-    headers: {
-      common: {
-        Accept: 'text/plain, */*',
-      },
-    },
-  })
-
-  if (accessToken) {
-    entitiesApi.defaults.headers.common.Authorization = accessToken
-  }
-
-  entitiesApi.setBaseURL(process.env.ENTITY_API_HOST)
-
-  // Inject to context as entity
-  inject('entitiesApi', entitiesApi)
+  inject("mainApi", mainApi);
 }
